e494699e7b6776b5c26957a66f73650085dcc7ef
Abhishek Kumar <abhishekcusat21@gmail.com>
2025-08-10 13:15:37 +0530

    Time Limiter

---
+++ time-limiter/src/test/java/com/resilience/time_limiter/TimeLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resilience.time_limiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
